747f6b59866232efe2718d046a7f50878240f40c
Conor Wallace <conorw8@gmail.com>
2019-02-22 12:19:09 -0600

    added bin directory

--- src/rmTimeStamp.py
+++ src/rmTimeStamp.py
@@ -1,14 +1,14 @@
 import csv
 
-with open('new_data.csv', "r") as file_in:
+with open('../config/new_data.csv', "r") as file_in:
     with open('new_training_set.csv', "a") as file_out:
         writer = csv.writer(file_out)
         line_count = 0
         for row in csv.reader(file_in):
             if line_count == 0:
                 pass
-            elif row[1,6,11,16,21,26,31,36,41,46] == 0.0:
-                pass
+            #elif row[1,6,11,16,21,26,31,36,41,46] == 0.0:
+            #   pass
             else:
                 writer.writerow(row[1:])
             line_count += 1
