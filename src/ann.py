7fb23472f972e7bed3b1539ae5027e5e3b41edf5
Conor Wallace <conorw8@gmail.com>
2019-05-28 11:37:08 -0500

    ANN

--- src/ann.py
+++ src/ann.py
@@ -22,10 +22,8 @@ n_features = 4
 n_input = 10
 #size of each epoch (i.e. batch)
 batch_size = 64
-#number of hidden units in input layer lstm cell
-#hidden layer number of units
-#percentage to drop
-dropout = 0.3
+hidden1_units = 8
+hidden2_units = 8
 
 def read_data_sets(name):
     read_data_set = []
@@ -48,20 +46,20 @@ def generate_train_test_data_sets():
     #print(np.as_array(data_set.shape()))
     data = np.reshape(data_set, [int(len(data_set)/5), n_features+1])
     print(len(data))
-    data = np.reshape(data, [int(len(data)/n_input), n_input, n_features+1])
-    print(len(data))
+    #data = np.reshape(data, [int(len(data)/n_input), n_input, n_features+1])
+    print(data.shape)
     # shuffles the set of sequences
     np.random.shuffle(data)
 
     # takes the tire pressure classification from every 10th data point in every sequence
-    seg_label_data = data[:, n_input-1, n_features]
+    seg_label_data = data[:, n_features]
     # takes the feature data parameters from every sequence
-    seg_feature_data = data[:, :, :n_features]
+    seg_feature_data = data[:, :n_features]
 
     return np.asarray(seg_feature_data), np.asarray(seg_label_data)
 
 features, labels_norm = generate_train_test_data_sets()
-print(len(features))
+print(features.shape)
 labels = []
 # convert label data to one_hot arrays
 for k in range(0, len(labels_norm)):
@@ -70,14 +68,22 @@ for k in range(0, len(labels_norm)):
     labels = np.append(labels, one_hot_label)
 labels = np.reshape(labels, [-1, n_labels])
 
+print(labels.shape)
 # Create LSTM
 # expected input data shape: (batch_size, timesteps, data_dim)
 model = Sequential()
-model.add(LSTM(4, input_shape=(n_features, n_input), return_sequences=True))
+#Input Layer
+model.add(Dense(n_features, activation='relu'))
+#Hidden Layers
 model.add(Dropout(0.2))
-model.add(LSTM(4))
-model.add(Dense(n_labels, activation='sigmoid'))
+model.add(Dense(hidden1_units, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(hidden2_units, activation='relu'))
+model.add(Dropout(0.2))
+#Output Layer
+model.add(Dense(n_labels, activation='relu'))
 
+'''
 l=[]
 weights1=np.random.rand(10, 16) #weights
 weights2=np.random.rand(4, 16) #weights
@@ -102,43 +108,27 @@ y=[0.02566409, -0.02650933, -0.00851871] #array of biases
 l.append(weights1)
 l.append(np.asarray(y))
 model.layers[3].set_weights(np.asarray(l)) #loaded_model.layer[0] being the layer
-model.compile(optimizer, loss)
+'''
+#model.compile(optimizer, loss)
 #adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-history = model.fit(np.reshape(features, (features.shape[0], n_features, features.shape[1])),labels, batch_size=batch_size, epochs=num_epochs, validation_split= .3)
+#prediction = model.predict(np.reshape(features, (features.shape[0], n_features)))
+
+history = model.fit(np.reshape(features, (features.shape[0], n_features)),labels, batch_size=batch_size, epochs=num_epochs, validation_split= .3)
 
 weight_origin_0=model.layers[0].get_weights()
 weight_origin_1=model.layers[2].get_weights()
-weight_origin_2=model.layers[3].get_weights()
+weight_origin_2=model.layers[4].get_weights()
+weight_origin_2=model.layers[6].get_weights()
 
 print("model shape")
 print(np.asarray(model.layers).shape)
-print("LSTM Layer 1")
+print("Input Layer")
 print(np.asarray(weight_origin_0))
-print("LSTM Layer 2")
+print("Hidden Layer 1")
 print(np.asarray(weight_origin_1))
-print("Dense Layer")
+print("Hidden Layer 2")
 print(np.asarray(weight_origin_2))
-
-# summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
-model_yaml = model.to_yaml()
-with open("../config/model.yaml", "w") as yaml_file:
-    yaml_file.write(model_yaml)
-model.save_weights("../config/model.h5")
+print("Output Layer")
+print(np.asarray(weight_origin_3))
